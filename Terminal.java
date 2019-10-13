a4fe9450a4403659fab924aac290e5de5c132a79
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2019-10-13 07:14:14 +0200

    Styling fix

--- Terminal.java
+++ Terminal.java
@@ -94,7 +94,7 @@ public class Terminal{
                 exec = this.clear();
                 break;
             case DELETE_FILE:
-            	if(args.length == 0){
+                if(args.length == 0){
                     exec = this.rm(getCurrentDir());
                 }else if(args.length == 1){
                     exec = this.rm(args[0]);
@@ -218,22 +218,22 @@ public class Terminal{
         return exec;
     }
     @SuppressWarnings("unused")
-	Execution rm(String path) {
-    	Execution exec = new Execution();
-    	if(Paths.get(expandPath(path)).toFile().isDirectory()){
-    		final File folder = new File(expandPath(path));
-    		if(folder != null){
-    			exec.exit_code = Execution.ExitCode.READ_WRITE_ERROR;
+    Execution rm(String path) {
+        Execution exec = new Execution();
+        if(Paths.get(expandPath(path)).toFile().isDirectory()){
+            final File folder = new File(expandPath(path));
+            if(folder != null){
+                exec.exit_code = Execution.ExitCode.READ_WRITE_ERROR;
                 exec.output = folder.getName() + " can't be removed\n";
-    		}
-    		else{
-	            exec.exit_code = Execution.ExitCode.SUCCESS;
-	            exec.output = "";
-	            exec.output += folder.getName() + " was removed successfully\n";
-	            folder.delete();
-    		}
+            }
+            else{
+                exec.exit_code = Execution.ExitCode.SUCCESS;
+                exec.output = "";
+                exec.output += folder.getName() + " was removed successfully\n";
+                folder.delete();
+            }
         }
-    	else{
+        else{
             exec.exit_code = Execution.ExitCode.READ_WRITE_ERROR;
             exec.output = "Path specified is not a valid directory\n";
         }
